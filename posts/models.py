c75968920a6f7bc7dd5fe39a2dbe147b89315acb
Мария Лисицкая <lisitskayamari@gmail.com>
2021-02-26 16:23:32 +0000

    models and admin is OK

--- posts/models.py
+++ posts/models.py
@@ -4,13 +4,21 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
-)
-
-
-class Group(models.Model):
-    pass
+    )
+    group = models.ForeignKey(
+        Group, models.SET_NULL, blank=True, null=True,
+    )
